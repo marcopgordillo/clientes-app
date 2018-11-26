42483ebd070ed076489130b2acf3182ae802a3dd
marcopgordillo <marcopgordillo@gmail.com>
2018-11-26 15:48:03 -0500

    Implementando rutas y navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
