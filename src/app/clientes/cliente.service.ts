f1eeb70879ef9357a458209f0cce6762a2e24067
marcopgordillo <marcopgordillo@gmail.com>
2018-11-26 15:35:17 -0500

    Implementando Observable en ClienteServicio

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { Cliente } from './cliente.model';
 import { CLIENTES } from './clientes.json';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
 
 @Injectable()
 export class ClienteService {
 
-  getClientes(): Cliente[] {
-    return CLIENTES;
+  getClientes(): Observable<Cliente[]> {
+    return of(CLIENTES);
   }
 }
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Cliente } from './cliente.model';
 import { ClienteService } from './cliente.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
-export class ClientesComponent implements OnInit {
+export class ClientesComponent implements OnInit, OnDestroy {
 
   clientes: Cliente[];
+  subscription: Subscription;
 
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit() {
-    this.clientes = this.clienteService.getClientes();
+    this.subscription = this.clienteService.getClientes()
+    .subscribe(
+      (clientes: Cliente[]) => this.clientes = clientes);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
