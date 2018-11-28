ba16dae919e93fc954a35c00166949b50bd6aeae
marcopgordillo <marcopgordillo@gmail.com>
2018-11-28 09:39:12 -0500

    Agregando la ruta del paginador y el Observable ActivatedRoute

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,37 +1,43 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import swal from 'sweetalert2';
+import { tap } from 'rxjs/internal/operators';
+import { ActivatedRoute } from '@angular/router';
 
-import {Cliente} from './cliente.model';
-import {ClienteService} from './cliente.service';
-import {tap} from "rxjs/internal/operators";
+import { Cliente } from './cliente.model';
+import { ClienteService } from './cliente.service';
+
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
-export class ClientesComponent implements OnInit, OnDestroy {
+export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
-  subscription: Subscription;
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    let page = 0;
-    this.subscription = this.clienteService.getClientes(page)
-      .pipe(
-        tap(response => {
-          console.log('ClientesComponent: tap 3');
-          (response.content as Cliente[]).forEach(cliente => console.log(cliente.nombre));
-        })
-      )
-      .subscribe(
-        response => this.clientes = response.content as Cliente[]);
-  }
+    this.route.paramMap.subscribe(params => {
+      let page: number = +params.get('page');
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+      if (!page) {
+        page = 0;
+      }
+
+      this.clienteService.getClientes(page)
+        .pipe(
+          tap(response => {
+            console.log('ClientesComponent: tap 3');
+            (response.content as Cliente[]).forEach(cliente => console.log(cliente.nombre));
+          })
+        )
+        .subscribe(
+          response => this.clientes = response.content as Cliente[]);
+      }
+    );
+
   }
 
   delete(cliente: Cliente): void {
