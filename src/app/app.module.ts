4ab2ba659ed0da85c7bfd6a1f220dcc1dd7e2fc9
marcopgordillo <marcopgordillo@gmail.com>
2018-11-28 09:51:29 -0500

    Creando el componente paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {LOCALE_ID, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {DirectivaComponent} from './directiva/directiva.component';
-import {ClientesComponent} from './clientes/clientes.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ClienteService} from './clientes/cliente.service';
-import {FormsModule} from '@angular/forms';
-import {FormComponent} from './clientes/form.component';
-import localeEs from "@angular/common/locales/es";
-import {registerLocaleData} from "@angular/common";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ClienteService } from './clientes/cliente.service';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './clientes/form.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { PaginatorComponent } from './paginator/paginator.component';
 
 registerLocaleData(localeEs, 'es');
 
@@ -23,7 +24,8 @@ registerLocaleData(localeEs, 'es');
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
-    FormComponent
+    FormComponent,
+    PaginatorComponent
   ],
   imports: [
     BrowserModule,
