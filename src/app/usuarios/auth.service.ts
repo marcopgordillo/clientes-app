33b9a59cb7c815b594155acef2fdeb77e6557f0d
marcopgordillo <marcopgordillo@gmail.com>
2018-12-19 18:00:39 -0500

    Guardado los datos de usuario y token en el sessionStorage.

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -6,14 +6,37 @@ import { Usuario } from './usuario';
 @Injectable()
 export class AuthService {
 
+  private _usuario: Usuario;
+  private _token: string;
+
   constructor(private http: HttpClient) { }
+
+  public get usuario(): Usuario {
+    if (this._usuario != null) {
+      return this._usuario;
+    } else if (this._usuario == null && sessionStorage.getItem('usuario') != null) {
+      this._usuario = JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+      return this._usuario;
+    }
+    return new Usuario();
+  }
+
+  public get token(): string {
+    if (this._token != null) {
+      return this._token;
+    } else if (this._token == null && sessionStorage.getItem('token') != null) {
+      this._token = sessionStorage.getItem('token');
+      return this._token;
+    }
+    return null;
+  }
 
   login(usuario: Usuario): Observable<any> {
     const urlEndpoint = 'http://localhost:8080/oauth/token';
     const credenciales = btoa('angularapp' + ':' + '12345'); // Se encripta con base64 (btoa)
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic' + credenciales
+      'Authorization': 'Basic ' + credenciales
     });
 
     const params = new URLSearchParams();
@@ -21,6 +44,33 @@ export class AuthService {
     params.set('username', usuario.username);
     params.set('password', usuario.password);
 
-    return this.http.post<any>(urlEndpoint, params, {headers: httpHeaders});
+    console.log(params.toString());
+
+    return this.http.post<any>(urlEndpoint, params.toString(), {headers: httpHeaders});
+  }
+
+  guardarUsuario(accessToken: string): void {
+    const payload = this.obtenerDatosToken(accessToken);
+    this._usuario = new Usuario();
+    this._usuario.username = payload.user_name;
+    this._usuario.nombre = payload.nombre;
+    this._usuario.apellido = payload.apellido;
+    this._usuario.email = payload.email;
+    this._usuario.roles = payload.authorities;
+
+    console.log(this._usuario);
+    sessionStorage.setItem('usuario', JSON.stringify(this._usuario));
+  }
+
+  guardarToken(accessToken: string): void {
+    this._token = accessToken;
+    sessionStorage.setItem('token', this._token);
+  }
+
+  private obtenerDatosToken(accessToken: string): any {
+    if (accessToken != null) {
+      return JSON.parse(atob(accessToken.split('.')[1]));
+    }
+    return null;
   }
 }
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import swal from 'sweetalert2';
 
 import { Usuario } from './usuario';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   titulo = 'Por favor Sign In!';
   usuario: Usuario;
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
     this.usuario = new Usuario();
@@ -23,6 +26,19 @@ export class LoginComponent implements OnInit {
     if (this.usuario.username == null || this.usuario.password == null) {
       swal('Error Login', 'Username o password vacías!', 'error');
     }
+
+    this.authService.login(this.usuario).subscribe(response => {
+
+      this.authService.guardarUsuario(response.access_token);
+      this.authService.guardarToken(response.access_token);
+
+      const usuarioAuth = this.authService.usuario;
+
+      console.log(usuarioAuth);
+
+      this.router.navigate(['/clientes']);
+      swal('Login', `Hola ${usuarioAuth.username}, has iniciado sesión con éxito!`, 'success');
+    });
   }
 
 }
