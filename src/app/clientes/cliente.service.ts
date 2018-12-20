059c227a9a1a3679b48f80944a8052953ef86c4e
marcopgordillo <marcopgordillo@gmail.com>
2018-12-20 17:55:12 -0500

    Cerrar session en angular cuando haya expirado el token en el backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -136,6 +136,11 @@ export class ClienteService {
 
   private isNoAutorizado(e): boolean {
     if (e.status === 401) {
+      
+      if (this.authService.isAuthenticated()) {
+        this.authService.logout();
+      }
+      
       this.router.navigate(['/login']);
       return true;
     }
