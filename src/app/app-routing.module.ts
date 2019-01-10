0f2a17dbfdbab848e6975ea8beb9958f8472233e
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-10 08:15:57 -0500

    Añadiendo seguridad en las rutas con guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component';
-import { FormComponent } from './clientes/form/form.component';
-import { LoginComponent } from './usuarios/login.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DirectivaComponent} from './directiva/directiva.component';
+import {ClientesComponent} from './clientes/clientes.component';
+import {FormComponent} from './clientes/form/form.component';
+import {LoginComponent} from './usuarios/login.component';
+import {AuthGuard} from './usuarios/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full' },
   { path: 'directivas', component: DirectivaComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'clientes/page/:page', component: ClientesComponent },
-  { path: 'clientes/form', component: FormComponent },
-  { path: 'clientes/form/:id', component: FormComponent },
+  { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent }
 ];
 
