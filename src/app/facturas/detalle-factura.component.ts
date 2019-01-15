c195f54d42880811f8cf6f18c5ea5db690cfe4b1
Marco Gordillo <marcopgordillo@gmail.com>
2019-01-14 23:30:49 -0500

    Anadido el detalle de la factura

--- src/app/facturas/detalle-factura.component.ts
+++ src/app/facturas/detalle-factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FacturaService } from './services/factura.service';
+import { Factura } from './models/factura.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-factura',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleFacturaComponent implements OnInit {
 
-  constructor() { }
+  factura: Factura;
+  titulo = 'Factura';
+
+
+  constructor(private facturaService: FacturaService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const id = +params.get('id');
+      this.facturaService.getFactura(id).subscribe(factura => this.factura = factura);
+    });
   }
 
 }
