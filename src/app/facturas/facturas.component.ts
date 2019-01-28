4e2fbf70e378d94839531cc6b4f77c9bb54ae3ce
marcopgordillo <marcopgordillo@gmail.com>
2019-01-28 11:45:48 -0500

    Validado formulario de la factura. Metodo 2

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -109,10 +109,17 @@ export class FacturasComponent implements OnInit {
     this.factura.items = this.factura.items.filter((item: ItemFactura) => id !== item.producto.id);
   }
 
-  create(): void {
-    this.facturaService.create(this.factura).subscribe(factura => {
-      swal(this.titulo, `Factura ${factura.descripcion} creada con éxito!`, 'success');
-      this.router.navigate(['/clientes']);
-    });
+  create(facturaForm: any): void {
+
+    if (this.factura.items.length == 0) {
+      this.autoCompleteControl.setErrors({'invalid': true});
+    }
+
+    if (facturaForm.form.valid && this.factura.items.length > 0) {
+      this.facturaService.create(this.factura).subscribe(factura => {
+        swal(this.titulo, `Factura ${factura.descripcion} creada con éxito!`, 'success');
+        this.router.navigate(['/clientes']);
+      });
+    }
   }
 }
